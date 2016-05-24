14a2aaa83358f69d2c32b17ae2bed365dde927f1
gtsntzs <gtsntzs@gmail.com>
2016-05-24 13:49:28 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
+lass Contact < ActiveRecord::Base
+  self.table_name = 'salesforce.contact'
+end
+
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
+end
 
 get "/" do
   erb :home
